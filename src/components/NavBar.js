c43a656d2c08aaeae7c1da19a11e08d53b60bf3c
Ratmir <rklimow409@gmail.com>
2022-04-23 02:33:24 +0300

    Added NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from "@chakra-ui/react";
+
+const NavBar = () => {
+    return (
+        <Container maxW={{base: '1310px'}}>
+            123123123123
+        </Container>
+    )
+}
+
+export default NavBar
